7d651875bd5d8289bb8a1411a86a037be304c661
Yash <yash.mehta@jklgroup.in>
2023-09-25 18:15:30 +0530

    created cart repository

---
+++ src/cart/repository/cart.repository.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CartEntity } from '../entities';
+import { Repository } from 'typeorm/repository/Repository';
+import { NotFoundException } from '../errors';
+
+export interface ICartRepository {
+  getCart(userId: string): Promise<CartEntity[] | null>;
+  addItem(userId: string, productId: string, quantity: number): Promise<void>;
+  removeItemFromCart(
+    userId: string,
+    productId: string,
+    quantity: number,
+  ): Promise<void>;
+  deleteCartItems(userId: string): Promise<void>;
+}
+
+@Injectable()
+export class CartRepository implements ICartRepository {
+  constructor(
+    @InjectRepository(CartEntity)
+    private readonly cartEntity: Repository<CartEntity>,
+  ) {}
+
+  async getCart(userId: string): Promise<CartEntity[] | null> {
+    return await this.cartEntity
+      .createQueryBuilder('cart')
+      .innerJoin('cart.user', 'user')
+      .where('user.id = :userId', { userId })
+      .getMany();
+  }
+
+  async addItem(
+    userId: string,
+    productId: string,
+    quantity: number,
+  ): Promise<void> {
+    let cartItem = await this.cartEntity
+      .createQueryBuilder('cart')
+      .innerJoinAndSelect('cart.user', 'user')
+      .innerJoinAndSelect('cart.product', 'product')
+      .where('user.id = :userId', { userId })
+      .andWhere('product.id = :productId', { productId })
+      .getOne();
+    if (cartItem) {
+      cartItem.quantity += quantity;
+    } else {
+      cartItem = this.cartEntity.create({
+        user: { id: userId },
+        product: { id: productId },
+        quantity: quantity,
+      });
+    }
+    await cartItem.save();
+  }
+
+  async removeItemFromCart(
+    userId: string,
+    productId: string,
+    quantity: number,
+  ): Promise<void> {
+    const cartItem = await this.cartEntity
+      .createQueryBuilder('cart')
+      .innerJoinAndSelect('cart.user', 'user')
+      .innerJoinAndSelect('cart.product', 'product')
+      .where('user.id = :userId', { userId })
+      .andWhere('product.id = :productId', { productId })
+      .getOne();
+    if (!cartItem) {
+      throw new NotFoundException();
+    }
+    cartItem.quantity -= quantity;
+    if (cartItem.quantity > 0) {
+      await cartItem.save();
+    } else {
+      await this.cartEntity.remove(cartItem);
+    }
+  }
+
+  async deleteCartItems(userId: string): Promise<void> {
+    const cartItems = await this.cartEntity
+      .createQueryBuilder('cart')
+      .where('cart.userId = :userId', { userId })
+      .getMany();
+    await this.cartEntity.remove(cartItems);
+  }
+}
